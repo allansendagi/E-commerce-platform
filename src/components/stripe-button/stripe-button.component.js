88345a1f16c17ce6d4f2316603fd31b72cf841a5
allansendagi <sendagiallan87@gmail.com>
2019-08-26 17:27:36 +0400

    live hsoting on heroku and App.js minor changes

--- src/components/stripe-button/stripe-button.component.js
+++ src/components/stripe-button/stripe-button.component.js
@@ -2,29 +2,28 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
-	const priceForStripe = price * 100;
-	const publishableKey = 'pk_test_XYozvx6n5p5J6sn7XlCzgCdb'
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_XYozvx6n5p5J6sn7XlCzgCdb';
 
-	const onToken = token => {
-		console.log(token);
-		alert('payment successful');
-	}
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
 
-	return (
-		<StripeCheckout
-		  label='pay Now'
-		  name='CRWN Clothing Ltd.'
-		  billingAddress
-		  shippingAddress
-		  image='https://res.cloudinary.com/allansendagi-com/image/upload/v1566664176/project/4.1_favicon.ico_i7ssox.ico'
-		  description={`Your total is $${price}`}
-		  amount={priceForStripe}
-		  panelLabel='pay Now'
-		  token={onToken}
-		  stripeKey={publishableKey}
-		/>
-	);
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
 
 export default StripeCheckoutButton;
-
