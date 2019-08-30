3829f29c0a911a28b71acd40d117c598113d5fe5
allansendagi <sendagiallan87@gmail.com>
2019-08-30 22:23:18 +0400

    adding HighOrderComponent and a spinner to lead while fetching data from our backend

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,9 +4,17 @@ import { connect } from 'react-redux';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 import { firestore, convertCollectionsSnapShotToMap } from '../../firebase/firebase.utils';
+import WithSpinner from '../../components/with-spinner/with-spinner.component'
 import { updateCollections} from '../../redux/shop/shop.actions';
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+	state = {
+		loading: true
+	}
+
 	unsubscribeFromSnapshot = null;
 
 	componentDidMount() {
@@ -16,16 +24,20 @@ class ShopPage extends React.Component {
 		this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
 			const collectionsMap = convertCollectionsSnapShotToMap(snapshot)
 			updateCollections(collectionsMap);
+			this.setState({loading: false });
 		});
 	}
 
 
 	render() {
 		const { match } = this.props
+		const { loading } = this.state;
 	return (
 		<div className='shop-page'>
-		   <Route exact path={`${match.path}`} component={CollectionsOverview} />
-		   <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+		   <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>} />
+		   <Route path={`${match.path}/:collectionId`} 
+		   render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props }/>}
+		   />
 	    </div>
 	  )
 	}
